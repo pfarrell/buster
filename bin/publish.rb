c629e9727a82af902b44080863f2bf898e59b14f
Patrick Farrell <pfarrell@prosper.com>
2014-07-09 18:10:35 -0700

    removed dependecy on tweets for mocked objects

--- bin/publish.rb
+++ bin/publish.rb
@@ -2,17 +2,20 @@ require './lib/buster'
 require 'redis'
 require 'json'
 
+$connected = ARGV[0] != "offline"
 $redis = Redis.new
 $urls = "tweets:urls"
 $raw = "tweets:raw"
 $tagged = "tweets:tagged"
 
 def broadcast(spy, tweet)
+  puts tweet
   json = tweet.to_json
   $redis.publish($raw, json)
-  $redis.publish($urls, json) if tweet.urls.size > 0
-  $redis.publish($tagged, json) if tweet.hashtags.size > 0
+  $redis.publish($urls, json) if json["urls"] && json["urls"].size > 0
+  $redis.publish($tagged, json) if json["hashtags"] && json["hashtags"].size > 0
 end
 
-bus = Spy.new
+bus = $connected ? Spy.new : SpyMock.new("spec/fixtures/tweets_redacted.json")
+
 bus.sample{|spy, tweet| broadcast(spy,tweet)}
