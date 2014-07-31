ea554b68001b9010577696a750290193eabe902d
Patrick Farrell <mr.patfarrell@gmail.com>
2014-07-31 07:44:01 -0700

    stats collector

---
+++ bin/stats.rb
@@ -0,0 +1,35 @@
+require 'json'
+require 'timetrap'
+require 'redis'
+
+def output(method, dict, ctr)
+  method.print "\n#{Time.now.to_s} calculating retweets from #{ctr} tweets [#{dict.count}]\n"
+  dict.top(25).each do |tweet|
+    method.print "#{tweet}\n"
+  end
+end
+
+$r = Redis.new
+ctr = 0
+dict = TimeTrap.new
+
+begin
+
+  pubsub = Thread.new {
+    $r.subscribe "tweets:raw" do |on|
+      on.message do |channel, msg|
+        ctr += 1
+        next if msg.nil? || msg.size < 2
+        tweet = JSON.parse(msg)
+        dict.add(tweet["text"])
+        output($stderr, dict, ctr) if(ctr % 500 == 0)
+        #dict.delete_if {|key,val| val.count < 3 } if(ctr % 5000 == 0)
+      end
+    end
+  }
+
+  pubsub.join
+rescue SignalException => ex
+  puts ""
+end
+puts ctr
--- buster.gemspec
+++ buster.gemspec
@@ -26,6 +26,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "thin"
   spec.add_dependency 'capistrano', '~> 3.2.0'
   spec.add_dependency 'capistrano-bundler', '~> 1.1.2'
+  spec.add_dependency 'time_trap'
 
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
